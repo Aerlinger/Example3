8a60d77af7c23137d4b16298c4f7ccbbe885a645
Anthony Erlinger <aerlinger@gmail.com>
2014-01-12 21:43:23 -0500

    Fix in predict.m

--- predict.m
+++ predict.m
@@ -21,9 +21,9 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
-X_new = [ones(m, 1) X];
+a1 = [ones(m, 1) X];
 
-z2 = Theta1*X_new';
+z2 = Theta1*a1';
 a2 = sigmoid(z2);
 a2 = [ones(1, size(a2, 2)); a2];
 
